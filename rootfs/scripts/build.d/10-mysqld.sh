790a99605ee3e7d71621c17528415f2ed37ccea0
Sudhaker Raj <sudhaker@build-03.lab.sudhaker.com>
2017-08-02 18:49:04 -0400

    first commit fixed build repo

--- rootfs/scripts/build.d/10-mysqld.sh
+++ rootfs/scripts/build.d/10-mysqld.sh
@@ -1,6 +1,4 @@
 
-sed -i 's/dl-cdn.alpinelinux.org/local.sudhaker.com/' /etc/apk/repositories
-
 apk add --no-cache mariadb-common libaio ncurses-libs libstdc++ pwgen
 
 apk add --no-cache mariadb mariadb-client
@@ -10,7 +8,5 @@ tar cf /tmp/mariadb-light.tar /usr/bin/mysqld /usr/bin/mysql_install_db /usr/bin
 apk del mariadb mariadb-client
 
 tar xf /tmp/mariadb-light.tar
-
-sed -i 's/local.sudhaker.com/dl-cdn.alpinelinux.org/' /etc/apk/repositories
 
 rm -fr /tmp/* /var/cache/apk/*
