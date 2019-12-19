084ae48767b2d4145f2399142b0d84856a4d8b76
Sudhaker Raj <sudhaker@centos7.localdomain>
2019-12-19 18:07:28 -0500

    latest version

--- rootfs/scripts/build.d/10-mysqld.sh
+++ rootfs/scripts/build.d/10-mysqld.sh
@@ -1,9 +1,9 @@
 
-apk add --no-cache mariadb-common libaio ncurses-libs libstdc++ pwgen
+apk add --no-cache mariadb-common libaio ncurses-libs libstdc++ pwgen pcre xz-libs
 
 apk add --no-cache mariadb mariadb-client
 
-tar cf /tmp/mariadb-light.tar /usr/bin/mysqld /usr/bin/mysql_install_db /usr/bin/my_print_defaults /usr/share/mysql/ /usr/bin/mysql /usr/bin/mysqldump
+tar cf /tmp/mariadb-light.tar /usr/bin/mysqld /usr/bin/mysql_install_db /usr/bin/my_print_defaults /usr/share/mariadb/ /usr/bin/mysql /usr/bin/mysqldump
 
 apk del mariadb mariadb-client
 
--- rootfs/scripts/wrapper/mysqld.sh
+++ rootfs/scripts/wrapper/mysqld.sh
@@ -12,7 +12,7 @@ else
 
     chown -R mysql:mysql /var/lib/mysql
 
-    mysql_install_db --user=mysql --force > /dev/null
+    mysql_install_db --user=mysql --datadir='/var/lib/mysql' --force > /dev/null
 
     if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
         MYSQL_ROOT_PASSWORD=$(pwgen 16 1)
@@ -55,8 +55,8 @@ EOF
         fi
     fi
 
-    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+    /usr/bin/mysqld --user=mysql --datadir='/var/lib/mysql' --bootstrap --verbose=0 < $tfile
     rm -f $tfile
 fi
 
-exec /usr/bin/mysqld --user=mysql --console
+exec /usr/bin/mysqld --user=mysql --datadir='/var/lib/mysql' --console
